4e8a2a0048e6375c188793f0aec83db840736bd9
ray <raygoodup@gmail.com>
2020-11-22 14:19:15 +0800

    LineCounter增加注释

--- CoroutineAdvanced/src/main/java/com/bennyhuo/kotlin/coroutines/advanced/eg/LineCounter.kt
+++ CoroutineAdvanced/src/main/java/com/bennyhuo/kotlin/coroutines/advanced/eg/LineCounter.kt
@@ -14,6 +14,9 @@ import java.util.concurrent.Executors
 
 val KotlinFileFilter = { file: File -> file.isDirectory || file.name.endsWith(".kt") }
 
+/**
+ * 数据类
+ */
 data class FileLines(val file: File, val lines: Int) {
     override fun toString(): String {
         return "${file.name}: $lines"
@@ -25,13 +28,19 @@ suspend fun main() {
     log(result)
 }
 
+/**
+ * 使用suspend的挂起函数
+ * 传入一个文件，返回一个hashMap
+ */
 suspend fun lineCounter(root: File): HashMap<File, Int> {
-    return Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() + 1).asCoroutineDispatcher()
+    return Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() + 1)
+        .asCoroutineDispatcher()
+        //use自动关闭流
         .use {
-            withContext(it){
-                val fileChannel  = walkFile(root)
+            withContext(it) {
+                val fileChannel = walkFile(root)
 
-                val fileLinesChannels = List(5){
+                val fileLinesChannels = List(5) {//开启5个协程
                     fileLineCounter(fileChannel)
                 }
 
@@ -40,23 +49,27 @@ suspend fun lineCounter(root: File): HashMap<File, Int> {
         }
 }
 
-fun CoroutineScope.walkFile(root: File): ReceiveChannel<File> {
-    return produce(capacity = Channel.BUFFERED) {
-        fileWalker(root)
-    }
-}
 
 suspend fun SendChannel<File>.fileWalker(file: File) {
-    if(file.isDirectory){
+    if (file.isDirectory) {
         file.listFiles()?.filter(KotlinFileFilter)?.forEach { fileWalker(it) }
     } else {
         send(file)
     }
 }
 
+/**
+ * Scope的扩展方法，创建
+ */
+fun CoroutineScope.walkFile(root: File): ReceiveChannel<File> {
+    return produce(capacity = Channel.BUFFERED) {
+        fileWalker(root)
+    }
+}
+
 fun CoroutineScope.fileLineCounter(input: ReceiveChannel<File>): ReceiveChannel<FileLines> {
     return produce(capacity = Channel.BUFFERED) {
-        for (file in input){
+        for (file in input) {
             file.useLines {
                 send(FileLines(file, it.count()))
             }
@@ -67,6 +80,7 @@ fun CoroutineScope.fileLineCounter(input: ReceiveChannel<File>): ReceiveChannel<
 suspend fun CoroutineScope.resultAggregator(channels: List<ReceiveChannel<FileLines>>): HashMap<File, Int> {
     val map = HashMap<File, Int>()
     channels.aggregate { filteredChannels ->
+        //知识点
         select<FileLines?> {
             filteredChannels.forEach {
                 it.onReceiveOrNull {
@@ -74,7 +88,7 @@ suspend fun CoroutineScope.resultAggregator(channels: List<ReceiveChannel<FileLi
                     it
                 }
             }
-        } ?.let {
+        }?.let {
             map[it.file] = it.lines
         }
     }
@@ -83,5 +97,7 @@ suspend fun CoroutineScope.resultAggregator(channels: List<ReceiveChannel<FileLi
 
 tailrec suspend fun List<ReceiveChannel<FileLines>>.aggregate(block: suspend (List<ReceiveChannel<FileLines>>) -> Unit) {
     block(this)
-    filter { !it.isClosedForReceive }.takeIf { it.isNotEmpty() }?.aggregate(block)
+    filter {
+        !it.isClosedForReceive//过滤掉已经关闭的
+    }.takeIf { it.isNotEmpty() }?.aggregate(block)
 }
